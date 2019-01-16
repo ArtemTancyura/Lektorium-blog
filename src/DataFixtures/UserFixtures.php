1a25b156ab1b1c4f56e2deb9c757314c1255138c
Artem Tantsiura <artemtancyura.sokil.che@gmail.com>
2019-01-16 02:30:46 +0200

    Update UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -7,7 +7,6 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
 class UserFixtures extends Fixture
 {
@@ -20,31 +19,17 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user1 = new User();
-        $encodedPassword1 = $this->passwordEncoder->encodePassword($user1, 'blogger');
-        $user1
-            ->setRoles(['ROLE_BLOGGER', 'ROLE_USER'])
-            ->setEmail('blog@blog.blog')
-            ->setPassword($encodedPassword1)
-            ->setFirstName('Notartem')
-            ->setLastName('Nottantsiura')
+        $user = new User();
+        $faker = \Faker\Factory::create();
+        $encodedPassword = $this->passwordEncoder->encodePassword($user, 'user');
+        $user
+            ->setRoles(['ROLE_USER'])
+            ->setEmail('user@user.user')
+            ->setPassword($encodedPassword)
+            ->setFirstName($faker->firstName)
+            ->setLastName($faker->lastName)
             ->setApiToken($uuid4 = Uuid::uuid4());
-        $manager->persist($user1);
-        $manager->flush();
-
-        for ($i = 0; $i < 5; $i++) {
-            $user2 = new User();
-            $faker = \Faker\Factory::create();
-            $encodedPassword2 = $this->passwordEncoder->encodePassword($user2, $faker->realText(10));
-            $user2
-                ->setRoles(['ROLE_USER'])
-                ->setEmail($faker->email)
-                ->setPassword($encodedPassword2)
-                ->setFirstName($faker->firstName)
-                ->setLastName($faker->lastName)
-                ->setApiToken($uuid4 = Uuid::uuid4());
-            $manager->persist($user2);
-        }
+        $manager->persist($user);
         $manager->flush();
     }
 }
